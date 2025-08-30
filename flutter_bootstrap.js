e8643d940d228b341140eac8074f36e0b235981d
benhook1013 <ben.hook@hotmail.com>
2025-08-30 13:34:54 +0000

    Deploying to gh-pages from @ benhook1013/space-game@b4aafa02a5b2f2114590df3c61ce6e6f2f9adbc6 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"ef0cd000916d64fa0c5d09cc809fa7ad244a57
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1115814232"
+    serviceWorkerVersion: "2696706477"
   }
 });
