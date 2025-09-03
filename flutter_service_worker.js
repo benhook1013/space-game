3c1bec6f2398e2915153fa163a750f573f148d66
benhook1013 <ben.hook@hotmail.com>
2025-09-03 09:13:09 +0000

    Deploying to gh-pages from @ benhook1013/space-game@2d9c457ed02e9857b7aa21815e5fe9f8c5f71347 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -51,11 +51,12 @@ const RESOURCES = {"segmenter_polyfill.js": "42e980438259612fd275ba3eb9a06067",
 "assets/AssetManifest.bin": "8e951d096130feaf72994b40c373b3cd",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "33b7d9392238c04c131b6ce224e13711",
-"flutter_bootstrap.js": "bfb8cf91683a592ed91a8287fa1877ba",
+"flutter_bootstrap.js": "933c519796d7b5e65364ef9e4b167f25",
 "assets_manifest.json": "49d04580c0dcd38aa40a6a9f043d0a34",
 "sw.js": "f5af813e8d82c3ae48671c8f2c7caab3",
-"index.html": "bf74b98cc4276fc2496c7488cf0d8467",
-"/": "bf74b98cc4276fc2496c7488cf0d8467"};
+"favicon.png": "6ef9dfb7dc912ce20411ecf920217d46",
+"index.html": "a502fd2ee740784298567e1eb3e64ad2",
+"/": "a502fd2ee740784298567e1eb3e64ad2"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
