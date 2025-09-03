88003bb0d33f480f0e72bd45352cc24941611169
Ben Hook <ben.hook@hotmail.com>
2025-09-03 22:11:11 +1200

    fix: update joystick scale when paused

--- lib/game/space_game.dart
+++ lib/game/space_game.dart
@@ -318,10 +318,9 @@ class SpaceGame extends FlameGame
   }
 
   void _updateJoystickScale() {
-    final oldJoystick = joystick;
-    joystick = _buildJoystick();
-    oldJoystick.removeFromParent();
-    add(joystick);
+    final scale = settingsService.joystickScale.value;
+    (joystick.knob as CircleComponent).radius = 20 * scale;
+    (joystick.background as CircleComponent).radius = 50 * scale;
   }
 
   void _updateHudButtonScale() {
