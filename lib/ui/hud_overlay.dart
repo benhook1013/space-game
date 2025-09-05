a99eb5bf69c1b69b874c22cc4846b37b53a55bef
Ben Hook <ben.hook@hotmail.com>
2025-09-05 19:27:43 +1200

    Reorder HUD pillboxes

--- lib/ui/hud_overlay.dart
+++ lib/ui/hud_overlay.dart
@@ -43,12 +43,12 @@ class HudOverlay extends StatelessWidget {
                         const SizedBox(width: 8),
                         Padding(
                           padding: const EdgeInsets.only(top: 8),
-                          child: MineralDisplay(game: game),
+                          child: HealthDisplay(game: game),
                         ),
                         const SizedBox(width: 8),
                         Padding(
                           padding: const EdgeInsets.only(top: 8),
-                          child: HealthDisplay(game: game),
+                          child: MineralDisplay(game: game),
                         ),
                       ],
                     ),
