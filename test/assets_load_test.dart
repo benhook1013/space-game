a2e9f1653003261d5ed2a80f36f59f8a9475ea8a
Ben Hook <ben.hook@hotmail.com>
2025-09-06 16:07:07 +1200

    test: add asset loading coverage

---
+++ test/assets_load_test.dart
@@ -0,0 +1,35 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flame/flame.dart';
+import 'package:flame_audio/flame_audio.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:space_game/assets.dart';
+
+class _FakeAudioCache extends AudioCache {
+  @override
+  Future<List<Uri>> loadAll(List<String> files) async => [];
+
+  @override
+  Future<void> clearAll() async {}
+}
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {
+    Flame.images.clearCache();
+    FlameAudio.audioCache = _FakeAudioCache();
+  });
+
+  test('Assets.load can be called multiple times', () async {
+    await Assets.load();
+    await Assets.load();
+  });
+
+  test('loading a missing asset throws', () {
+    expect(
+      () => Flame.images.fromCache('does_not_exist.png'),
+      throwsA(isA<AssertionError>()),
+    );
+  });
+}
