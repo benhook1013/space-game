36843ba098c48be3b3ad393d301d09867fa92c1b
Ben Hook <ben.hook@hotmail.com>
2025-09-08 21:54:30 +1200

    chore: hide object pool internals

--- lib/util/object_pool.dart
+++ lib/util/object_pool.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 /// Generic object pool to minimise allocations by reusing instances.
 class ObjectPool<T> {
   /// Creates a new pool that builds objects using [create].
@@ -11,8 +13,13 @@ class ObjectPool<T> {
   final int? maxSize;
   final List<T> _items = [];
 
-  /// Returns the cached items currently in the pool.
-  Iterable<T> get items => _items;
+  /// Returns an unmodifiable view of the cached items currently in the pool.
+  ///
+  /// Exposing the internal list directly would allow callers to modify it and
+  /// break the pool's bookkeeping. Returning an [UnmodifiableListView] keeps
+  /// the data read-only while still reflecting updates to the underlying list
+  /// without additional copying.
+  UnmodifiableListView<T> get items => UnmodifiableListView(_items);
 
   T acquire([void Function(T)? reset]) {
     final obj = _items.isNotEmpty ? _items.removeLast() : _create();
