e09bc46bdd11c5a9e93b9228aa113f98727b5f65
Ben Hook <ben.hook@hotmail.com>
2025-09-09 19:52:23 +1200

    Stub path provider for audio tests

--- lib/game/event_bus.dart
+++ lib/game/event_bus.dart
@@ -1,28 +1,32 @@
 import 'dart:async';
 
+/// Marker base class for all game events.
+abstract class GameEvent {}
+
 /// Simple event bus for broadcasting game lifecycle events.
 class GameEventBus {
-  final StreamController<Object> _controller =
-      StreamController<Object>.broadcast(sync: true);
+  final StreamController<GameEvent> _controller =
+      StreamController<GameEvent>.broadcast(sync: true);
 
   /// Emits an [event] to all listeners.
-  void emit(Object event) => _controller.add(event);
+  void emit(GameEvent event) => _controller.add(event);
 
   /// Returns a stream of events of type [T].
-  Stream<T> on<T>() => _controller.stream.where((e) => e is T).cast<T>();
+  Stream<T> on<T extends GameEvent>() =>
+      _controller.stream.where((e) => e is T).cast<T>();
 
   /// Closes the underlying stream controller.
   void dispose() => _controller.close();
 }
 
 /// Event fired when a component is spawned.
-class ComponentSpawnEvent<T> {
+class ComponentSpawnEvent<T> implements GameEvent {
   ComponentSpawnEvent(this.component);
   final T component;
 }
 
 /// Event fired when a component is removed.
-class ComponentRemoveEvent<T> {
+class ComponentRemoveEvent<T> implements GameEvent {
   ComponentRemoveEvent(this.component);
   final T component;
 }
