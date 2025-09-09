8fd119276f79703d023cf1af7a6a1d8f61c3f097
Ben Hook <ben.hook@hotmail.com>
2025-09-09 20:24:11 +1200

    feat: seal game events

--- test/event_bus_test.dart
+++ test/event_bus_test.dart
@@ -1,31 +1,24 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:space_game/game/event_bus.dart';
 
-class _IntEvent implements GameEvent {
-  _IntEvent(this.value);
-  final int value;
-}
-
-class _OtherEvent implements GameEvent {}
-
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
   test('emit delivers events to listeners', () {
     final bus = GameEventBus();
-    var received = 0;
-    bus.on<_IntEvent>().listen((e) => received = e.value);
-    bus.emit(_IntEvent(5));
-    expect(received, 5);
+    ComponentSpawnEvent<int>? received;
+    bus.on<ComponentSpawnEvent<int>>().listen((e) => received = e);
+    bus.emit(ComponentSpawnEvent<int>(5));
+    expect(received?.component, 5);
   });
 
   test('listeners only receive matching types', () {
     final bus = GameEventBus();
     final events = <int>[];
-    bus.on<_IntEvent>().listen((e) => events.add(e.value));
-    bus.emit(_IntEvent(1));
-    bus.emit(_OtherEvent());
-    bus.emit(_IntEvent(2));
+    bus.on<ComponentSpawnEvent<int>>().listen((e) => events.add(e.component));
+    bus.emit(ComponentSpawnEvent<int>(1));
+    bus.emit(ComponentSpawnEvent<String>('skip'));
+    bus.emit(ComponentSpawnEvent<int>(2));
     expect(events, [1, 2]);
   });
 }
