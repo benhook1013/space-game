5165f588c1292af85a77dd583780f129ff277cab
Ben Hook <ben.hook@hotmail.com>
2025-11-24 17:10:44 +1300

    Refactor lifecycle test setup

--- test/restart_lifecycle_test.dart
+++ test/restart_lifecycle_test.dart
@@ -1,51 +1,33 @@
 import 'dart:async';
 
 import 'package:flame/components.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import 'package:space_game/assets.dart';
 import 'package:space_game/components/explosion.dart';
 import 'package:space_game/components/player.dart';
 import 'package:space_game/constants.dart';
 import 'package:space_game/game/game_state.dart';
-import 'package:space_game/game/space_game.dart';
-import 'package:space_game/services/audio_service.dart';
-import 'package:space_game/services/storage_service.dart';
-import 'package:space_game/ui/game_over_overlay.dart';
-import 'package:space_game/ui/hud_overlay.dart';
-import 'package:space_game/ui/menu_overlay.dart';
-import 'package:space_game/ui/pause_overlay.dart';
-import 'test_images.dart';
+
+import 'helpers/lifecycle_test_helpers.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
   setUpAll(() async {
-    await loadTestImages([...Assets.players, ...Assets.explosions]);
+    await loadLifecycleTestImages();
   });
 
   setUp(() {
     SharedPreferences.setMockInitialValues({});
   });
 
-  Future<SpaceGame> createGame() async {
-    final storage = await StorageService.create();
-    final audio = await AudioService.create(storage);
-    audio.muted.value = true;
-    final game = SpaceGame(storageService: storage, audioService: audio);
-    game.overlays.addEntry(MenuOverlay.id, (_, __) => const SizedBox());
-    game.overlays.addEntry(HudOverlay.id, (_, __) => const SizedBox());
-    game.overlays.addEntry(PauseOverlay.id, (_, __) => const SizedBox());
-    game.overlays.addEntry(GameOverOverlay.id, (_, __) => const SizedBox());
-    await game.onLoad();
-    game.onGameResize(Vector2.all(100));
-    return game;
-  }
-
   test('restarting clears active explosions', () async {
-    final game = await createGame();
+    final game = await createLifecycleTestGame(
+      includeHudOverlay: true,
+      includePauseOverlay: true,
+      includeGameOverOverlay: true,
+    );
 
     await game.startGame();
     for (var i = 0; i < Constants.playerMaxHealth; i++) {
@@ -60,7 +42,11 @@ void main() {
   });
 
   test('restarting immediately clears pending explosions', () async {
-    final game = await createGame();
+    final game = await createLifecycleTestGame(
+      includeHudOverlay: true,
+      includePauseOverlay: true,
+      includeGameOverOverlay: true,
+    );
 
     await game.startGame();
     for (var i = 0; i < Constants.playerMaxHealth; i++) {
@@ -73,7 +59,11 @@ void main() {
   });
 
   test('restarting removes the previous player instance', () async {
-    final game = await createGame();
+    final game = await createLifecycleTestGame(
+      includeHudOverlay: true,
+      includePauseOverlay: true,
+      includeGameOverOverlay: true,
+    );
 
     await game.startGame();
     game.player.position.setValues(20, 20);
--- test/resume_game_engine_test.dart
+++ test/resume_game_engine_test.dart
@@ -1,37 +1,19 @@
 import 'package:flame/components.dart';
-import 'package:flame/flame.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import 'package:space_game/assets.dart';
-import 'package:space_game/game/space_game.dart';
-import 'package:space_game/services/audio_service.dart';
-import 'package:space_game/services/storage_service.dart';
-import 'package:space_game/ui/hud_overlay.dart';
-import 'package:space_game/ui/menu_overlay.dart';
-import 'package:space_game/ui/pause_overlay.dart';
+import 'helpers/lifecycle_test_helpers.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
   test('resumeGame resumes engine after external pause', () async {
     SharedPreferences.setMockInitialValues({});
-    await Flame.images.loadAll([
-      ...Assets.players,
-      ...Assets.enemies,
-      ...Assets.asteroids,
-      ...Assets.explosions,
-      Assets.bullet,
-    ]);
-    final storage = await StorageService.create();
-    final audio = await AudioService.create(storage);
-    final game = SpaceGame(storageService: storage, audioService: audio);
-    game.overlays.addEntry(MenuOverlay.id, (_, __) => const SizedBox());
-    game.overlays.addEntry(HudOverlay.id, (_, __) => const SizedBox());
-    game.overlays.addEntry(PauseOverlay.id, (_, __) => const SizedBox());
-    await game.onLoad();
-    game.onGameResize(Vector2.all(100));
+    await loadLifecycleTestImages();
+    final game = await createLifecycleTestGame(
+      includeHudOverlay: true,
+      includePauseOverlay: true,
+    );
     await game.startGame();
     await game.ready();
 
